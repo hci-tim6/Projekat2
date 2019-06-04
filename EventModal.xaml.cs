47fbe7c9a68b3de02b79f6bcd65248bf9f00013c
mdjokic <djokic.mihailo@gmail.com>
2019-06-04 18:15:27 +0200

    Implementiran donekle DatePicker

--- EventModal.xaml.cs
+++ EventModal.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Globalization;
+using System.Threading;
 
 namespace HCI_Projekat2
 {
@@ -213,6 +215,9 @@ namespace HCI_Projekat2
 
         public EventModal(Window owner, Event e, ObservableCollection<Tag> tags, ObservableCollection<Models.Type> types)
         {
+            CultureInfo ci = CultureInfo.CreateSpecificCulture(CultureInfo.CurrentCulture.Name);
+            ci.DateTimeFormat.ShortDatePattern = "dd.MM.yyyy.";
+            Thread.CurrentThread.CurrentCulture = ci;
             Owner = owner;
             DataContext = this;
             Tags = tags;
@@ -235,6 +240,8 @@ namespace HCI_Projekat2
             SelectedPrice = e.Price;
 
             InitializeComponent();
+            DateTime today = DateTime.Now;
+            TxtDatePicker.DisplayDateStart = today;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -256,6 +263,7 @@ namespace HCI_Projekat2
             Event.Space = (Space)TxtSpace.SelectedItem;
             Event.Price = (Price)TxtPrice.SelectedItem;
             Event.Type = (Models.Type)TxtType.SelectedItem;
+            Event.Date = (DateTime)TxtDatePicker.SelectedDate;
             Event.Tags = (ObservableCollection<Tag>)TxtTags.ItemsSource;
 
             Event.Icon = Event.Icon == null ? Event.Type.Icon : Event.Icon;
@@ -311,5 +319,7 @@ namespace HCI_Projekat2
             }
             this.View?.Refresh();
         }
+
+
     }
 }
